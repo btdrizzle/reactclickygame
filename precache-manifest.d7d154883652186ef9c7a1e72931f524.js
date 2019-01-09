494d468cad2f3d9da5d02a1055376b9cc3663299
btdrizzle <briandrake05@gmail.com>
2019-01-09 14:12:09 -0600

    Updates

---
+++ precache-manifest.d7d154883652186ef9c7a1e72931f524.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "961f2cf3bc081edce835",
+    "url": "/reactclickygame/static/js/runtime~main.961f2cf3.js"
+  },
+  {
+    "revision": "64964406383a16af8deb",
+    "url": "/reactclickygame/static/js/main.64964406.chunk.js"
+  },
+  {
+    "revision": "a3c0ded905873cbeab45",
+    "url": "/reactclickygame/static/js/1.a3c0ded9.chunk.js"
+  },
+  {
+    "revision": "64964406383a16af8deb",
+    "url": "/reactclickygame/static/css/main.6315b1cd.chunk.css"
+  },
+  {
+    "revision": "1b8be29b1993a5499249bcbe317e1cc6",
+    "url": "/reactclickygame/index.html"
+  }
+];
